e0bbb2fd7f542f5f4694dffa8a25a75baa87ddcb
Kostya Bunsberry <kostya.bunsberry@gmail.com>
2020-07-07 19:48:08 +0700

    Initial Commit

---
+++ HSEIntroduction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HSEIntroduction
+//
+//  Created by Kostya Bunsberry on 07.07.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
